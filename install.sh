6cbd422a44861e536f3b0c1d93b92b43d0aede77
Julien Bara <bara.julien@yahoo.fr>
2020-06-06 23:04:35 +0200

    add zsh config

--- install.sh
+++ install.sh
@@ -6,3 +6,15 @@ if echo $SHELL | grep -qv '/usr/bin/zsh'; then
 	chsh -s $(which zsh);
 fi
 
+# install zsh
+ZSH=~/.oh-my-zsh
+
+if [ -d "$ZSH" ]; then
+	echo "Oh My Zsh is already installed. Skipping.."
+else
+	echo "Installing Oh My Zsh..."
+	curl -L http://install.ohmyz.sh | sh
+fi
+
+# link zsh config
+ln -fs $(pwd)/.zshrc ~/.zshrc 
